99b21af9cbfaa1b3abf00ca5fe4240ab5aeb24f9
Harry <sahilbhaisharma1212@gmail.com>
2025-06-28 23:28:10 +0530

    Debugging Version clash of modules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,10 +16,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black overflow-x-hidden`}
-      >
-      
-      
+        className={`antialiased bg-black overflow-x-hidden`}
+      >      
         {children}
         <Toaster/>
       </body>
